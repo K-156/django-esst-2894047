bf28facfe08e251b21515aa14f0adae74fb911da
Lim Sze Ming Stephanie <limstephanie156@gmail.com>
2024-06-15 04:04:53 +0800

    added auth

--- home/views.py
+++ home/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from datetime import datetime
 
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
+@login_required(login_url="/admin")
 def home(request):
     return render(request, 'home/welcome.html', {'today':datetime.now()})
